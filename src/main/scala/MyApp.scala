6b0a7210c58637e591525c14cf44e14d549609ac
igor.yudnikov <1234qwerasdfT>
2017-09-22 16:50:54 +0300

    Init

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,10 +1,10 @@
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 object MyApp extends App {
 
-  type T = Vector[Option[String]]
 
-  val input: List[T] =
+  val input: List[Vector[Option[String]]] =
     List(
       Vector(Some("a1"), Some("a2"), Some("a3"), Some("a4")),
       Vector(Some("b1"), None, None, Some("b4")),
@@ -21,40 +21,95 @@ object MyApp extends App {
       Vector(None, Some("l2"), None, None)
     )
 
+  implicit val length = input.map(_.length).head
+
   val bool = input.map(t => (t.map(_.isDefined), t))
   val grouped = bool.groupBy(_._1)
   val keys = grouped.keys.toList
 
-  
+  trait Key {
+    val vector: Vector[Boolean]
+    val children: List[Key]
+    def flatten: List[Key] = {
+      if (children.nonEmpty) {
+        children.flatMap(_.flatten)
+      } else
+        this :: Nil
+    }
+    def merge(key: Key): Key = {
+      if (vector == key.vector)
+        withChildren(children.union(key.children))
+      else
+        withChildren(children.map(_.merge(key)))
+    }
+    def withChildren(children: List[Key]): Key
+  }
+
+  case class CompleteKey(val children: List[Key] = Nil)(implicit length: Int) extends Key {
+
+    override val vector: Vector[Boolean] = Vector.fill(length)(true)
+
+    override def withChildren(children: List[Key]): Key = copy(children = children)
+
+  }
+
+  case class NonCompleteKey(val vector: Vector[Boolean], val children: List[Key] = Nil) extends Key {
+
+    override def withChildren(children: List[Key]): Key = copy(children = children)
+
+  }
+
+  val keys0 = keys.partition(_.forall(_ == true))
+  val completeKeys0 = keys0._1.map(_ => new CompleteKey())
+  val uncompleteKeys = keys0._2.map(new NonCompleteKey(_))
 
   @tailrec
   def stairs[T](list: List[T], acc: List[(T, List[T])] = Nil): List[(T, List[T])] = {
-    if (list.tail.nonEmpty)
+    if (list.nonEmpty)
       stairs(list.tail, (list.head -> list.tail) :: acc)
     else
       acc
   }
 
-  val x = stairs(keys)
+  val s0 = stairs(uncompleteKeys)
 
-  def solveStairs[T](stairs: List[(T, List[T])], f: (T, T) => Boolean): List[(T, T)] = {
-    stairs.map { stair =>
-      stair._1 -> stair._2.filter {
-        f(stair._1, _)
+  val res: (List[CompleteKey], List[NonCompleteKey]) = {
+    s0.flatMap { stair =>
+      stair._2.map { k =>
+        stair._1 -> k
       }
-    } filter {
-      _._2.nonEmpty
-    } flatMap { t =>
-      t._2.map {
-        _ -> t._1
+    } collect {
+      case t if isComplete(t) => {
+        new CompleteKey(List(t._1, t._2))
+      }
+      case t if nonComplete(t) => {
+        println(t)
+        val res = new NonCompleteKey(simplify(t._1.vector, t._2.vector), List(t._1, t._2))
+        res
+      }
+    } partition {
+      _.isInstanceOf[CompleteKey]
+    }
+  }.asInstanceOf[(List[CompleteKey], List[NonCompleteKey])]
+
+  val completeKeys1 = completeKeys0.union(res._1).distinct
+  val x = res._2.partition(k => completeKeys0.flatMap(_.flatten).exists(_.vector == k.vector))
+
+  val m1 = res._1(2)
+  val m2 = res._2(3)
+  val m = m1.merge(m2)
+
+  null
+
+  def merge[T <: Key](to: List[Key], from: List[Key]): List[T] = {
+
+    for (t <- to) {
+      var buf = t
+      for (f <- from) {
+        buf = buf.merge(f)
       }
     }
   }
-
-  val solved = solveStairs(x, isComplete)
-  val semi = solveStairs(x, nonComplete)
-
-  val zs = semi.map(t => simplify(t._1, t._2) -> t)
 
   def simplify(a: Vector[Boolean], b: Vector[Boolean]): Vector[Boolean] = {
     val ab = a.zip(b)
@@ -63,14 +118,18 @@ object MyApp extends App {
 
   null
 
-  def isComplete: (Vector[Boolean], Vector[Boolean]) => Boolean = (a, b) => {
-    val ab = a.zip(b)
+  def isComplete(t: (NonCompleteKey, NonCompleteKey)): Boolean = isComplete(t._1, t._2)
+
+  def isComplete(a: NonCompleteKey, b: NonCompleteKey): Boolean = {
+    val ab = a.vector.zip(b.vector)
     val valid = !ab.exists(bb => bb._1 & bb._2)
     valid && ab.forall(bb => bb._1 | bb._2)
   }
 
-  def nonComplete: (Vector[Boolean], Vector[Boolean]) => Boolean = (a, b) => {
-    val ab = a.zip(b)
+  def nonComplete(t: (NonCompleteKey, NonCompleteKey)): Boolean = nonComplete(t._1, t._2)
+
+  def nonComplete(a: NonCompleteKey, b: NonCompleteKey): Boolean = {
+    val ab = a.vector.zip(b.vector)
     val valid = !ab.exists(bb => bb._1 & bb._2)
     valid && ab.exists(bb => !bb._1 & !bb._2)
   }
