3429b932607927298bb89ee22ac103e913d6a779
Don <igoryudnikov@gmail.com>
2017-09-24 21:03:26 +0300

    Fin...

--- src/main/scala/ru/yudnikov/trash/rows/NonCompleteRow.scala
+++
@@ -1,9 +0,0 @@
-package ru.yudnikov.trash.rows
-
-case class NonCompleteRow(value: Vector[Boolean], children: List[Row] = Nil, isRoot: Boolean = false) extends Row {
-
-  override val isComplete = false
-
-  override def withChildren(children: List[Row]): NonCompleteRow = copy(children = children)
-
-}
